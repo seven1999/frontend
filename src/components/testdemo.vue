34afe8b8d6e0752eb3da7663a10aaa66dfb146be
qi.song <songqi_0102@sina.com>
2019-06-03 00:03:19 +0800

    22

--- src/components/testdemo.vue
+++ src/components/testdemo.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>my first page</h1>
+  <div>
+    <h1 class="h1"
+        id="h1"
+        name="h1"
+        title="qi.song">my first page</h1>
+    <h2>my first page</h2>
+    <h3>my first page</h3>
+    <h4>my first page</h4>
+    <p>my first page</p>
+    <a href="http://www.runoob.com">这是一个链接</a> <br>
+    <a href="http://www.baidu.com">百度</a>
+  </div>
 </template>
